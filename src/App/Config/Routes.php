febdce5620ad460cc816a3cd6112c12c4a7bfa2b
lauraservettini <lauraservettini87@gmail.com>
2023-11-10 19:11:16 +0100

    add register page auth/register.php, /register route and RegisterController

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Controllers\{HomeController, AboutController};
+use App\Controllers\{HomeController, AboutController, RegisterController};
 
 function registerRoutes(App $app)
 {
     $app->get("/", [HomeController::class, "home"]);
 
     $app->get("/about", [AboutController::class, "about"]);
+
+    $app->get("/register", [RegisterController::class, "register"]);
 }
---
+++ src/App/Controllers/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+// use App\Config\Paths;
+
+class RegisterController
+{
+    public function __construct(
+        private TemplateEngine $view
+    ) {
+    }
+
+    public function register()
+    {
+        echo $this->view->render("/auth/register.php", [
+            "title" => "Register page",
+
+        ]);
+    }
+}
