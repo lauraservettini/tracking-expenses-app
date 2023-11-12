cdb12d91768dbb6d6690a935811e68c2aaaf6bb4
lauraservettini <lauraservettini87@gmail.com>
2023-11-12 18:02:00 +0100

    add table users to the database

--- database-cli.php
+++ database-cli.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . "/src/Framework/database.php";
+include __DIR__ . "/src/Framework/Database.php";
 
 use Framework\Database;
 
@@ -10,4 +10,31 @@ $db = new Database("mysql", [
     "dbname" => "expense_tracking_app"
 ], "root", "");
 
-echo "Connected to the database";
+$sqlFile = file_get_contents("./database.sql");
+
+$db->query($sqlFile);
+
+// try {
+//     $db->connection->beginTransaction();
+
+//     $search = "Value";
+
+//     // creare un "Prepared Statement" per evitare la SQL Injection
+//     // può essere usata o la key dell'array associativo o ?
+//     // $query = "SELECT * FROM products WHERE name=:name";
+
+//     // $stmt = $db->connection->prepare($query);
+
+//     // bindValue associa i parametri senza eseguire lo statement
+//     $stmt->bindValue("name", 'Gloves', PDO::PARAM_STR);
+
+//     $stmt->execute();ù
+
+//     $db->connection->commit();
+// } catch (Exception $error) {
+//     // verifica se la transaction è attiva
+//     if ($db->connection->inTransaction()) {
+//         $db->connection->rollBack();
+//     }
+//     echo "Transaction Failed";
+// }
--- src/Framework/database.php
+++ src/Framework/database.php
@@ -8,7 +8,7 @@ use PDO, PDOException;
 
 class Database
 {
-    public PDO $connection;
+    private PDO $connection;
 
     public function __construct(string $driver, array $config, string $username, string $password)
     {
@@ -23,5 +23,10 @@ class Database
         } catch (PDOException $e) {
             die("Unable to connect to database");
         }
+    }
+
+    public function query(string $query)
+    {
+        $this->connection->query($query);
     }
 }
