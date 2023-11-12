b9f65f300ae6b2ce880abd727aec22d2e750641d
lauraservettini <lauraservettini87@gmail.com>
2023-11-12 18:40:52 +0100

    add .env file and .env example

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,8 +6,13 @@ require __dir__ . "/../../vendor/autoload.php";
 
 use Framework\App;
 use App\Config\Paths;
+use Dotenv\Dotenv;
 
 use function App\Config\{registerRoutes, registerMiddleware};
+
+// carica .env
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
 $app = new App(Paths::SOURCE . "/App/container-definitions.php");
 
